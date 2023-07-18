f6f7d30bc58892e3368886dcce126f9706e05a93
Margulis162 <markmargulis@proton.me>
2023-07-17 21:39:23 -0400

    addet some basic styles so it does not hurt my eyes too much. applied another template literal with length method

--- js_str.js
+++ js_str.js
@@ -1,5 +1,12 @@
+//consts and vars
 let num = 123;
-num = num.toString();
+let str1 = "presto";
 const pOne = document.getElementById("one");
+const pTwo = document.getElementById("two");
+//paragraph one
+num = num.toString();
 console.log(num);
 pOne.textContent = (` Num contains ${num}. which is a ${typeof(num)}`);
+
+//paragraph two
+pTwo.textContent = ( `str1 contains "${str1}" which is  ${str1.length} char long`);
