f5af71112e588bb0abc33ff094acc77309c31f6a
Margulis162 <markmargulis@proton.me>
2023-07-18 20:56:56 -0400

    added third paragraph with a for loop braking a srting into single letters and making a ul out of them

--- js_str.js
+++ js_str.js
@@ -3,6 +3,8 @@ let num = 123;
 let str1 = "presto";
 const pOne = document.getElementById("one");
 const pTwo = document.getElementById("two");
+const pThree = document.getElementById("three");
+const list = document.querySelector("ul");
 //paragraph one
 num = num.toString();
 console.log(num);
@@ -10,3 +12,11 @@ pOne.textContent = (` Num contains ${num}. which is a ${typeof(num)}`);
 
 //paragraph two
 pTwo.textContent = ( `str1 contains "${str1}" which is  ${str1.length} char long`);
+
+//pharagraph three 
+for(i=0;i<str1.length; i++){
+    const listItem = document.createElement('li');
+    listItem.textContent = str1[i];
+    list.appendChild(listItem);
+    
+}
